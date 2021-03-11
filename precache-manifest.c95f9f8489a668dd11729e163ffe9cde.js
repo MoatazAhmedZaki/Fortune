071d7d6cc83bd94e3f255e9318b5ac935950a99a
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 10:26:57 +0200

    Updates

---
+++ precache-manifest.c95f9f8489a668dd11729e163ffe9cde.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e844b03aa317a73f5e7d8fc0279185ee",
+    "url": "/Fortune/index.html"
+  },
+  {
+    "revision": "01bc945cc1d12a8e313e",
+    "url": "/Fortune/static/css/main.8f0044ca.chunk.css"
+  },
+  {
+    "revision": "162c613c1374f495ff0e",
+    "url": "/Fortune/static/js/2.5fa9f7d9.chunk.js"
+  },
+  {
+    "revision": "01bc945cc1d12a8e313e",
+    "url": "/Fortune/static/js/main.7af5764f.chunk.js"
+  },
+  {
+    "revision": "3b301253564d22e2dc00",
+    "url": "/Fortune/static/js/runtime-main.6e2ec589.js"
+  },
+  {
+    "revision": "42fc11b7d81a742dcdcf70b43d1b0d13",
+    "url": "/Fortune/static/media/box3.42fc11b7.jpg"
+  },
+  {
+    "revision": "eb37dc3846a7a86afd0df328b112c6f6",
+    "url": "/Fortune/static/media/box5.eb37dc38.jpg"
+  },
+  {
+    "revision": "981f29a452ec1811281a6632c582132c",
+    "url": "/Fortune/static/media/car4.981f29a4.jpg"
+  },
+  {
+    "revision": "6ad44426d3c12f7cb64e42807ec26817",
+    "url": "/Fortune/static/media/car5.6ad44426.jpg"
+  },
+  {
+    "revision": "5cc029d25ab8a8d1ba97f0fa560afd81",
+    "url": "/Fortune/static/media/grid1.5cc029d2.jpg"
+  },
+  {
+    "revision": "3041f268016d8b0a9c0d3e44c8b10340",
+    "url": "/Fortune/static/media/grid2.3041f268.jpg"
+  },
+  {
+    "revision": "f28fa2f56bde82a89fbaf41d9959358a",
+    "url": "/Fortune/static/media/grid3.f28fa2f5.jpg"
+  },
+  {
+    "revision": "c84e9881c6b388ff172aac4521393470",
+    "url": "/Fortune/static/media/grid4.c84e9881.jpg"
+  },
+  {
+    "revision": "ba2dd065b3a9ceeac7d886a4d82f1da6",
+    "url": "/Fortune/static/media/grid5.ba2dd065.jpg"
+  },
+  {
+    "revision": "2a05f59d585ee132b6c1144948cb345c",
+    "url": "/Fortune/static/media/grid6.2a05f59d.jpg"
+  },
+  {
+    "revision": "9fbd79e6b9a15011b3a98a0f057da7a5",
+    "url": "/Fortune/static/media/item1.9fbd79e6.jpg"
+  },
+  {
+    "revision": "5832be9b124aecd1cba75ee268f49389",
+    "url": "/Fortune/static/media/live.5832be9b.jpg"
+  },
+  {
+    "revision": "ea3226d90b1f1edbaa78c241f8772393",
+    "url": "/Fortune/static/media/logo.ea3226d9.png"
+  },
+  {
+    "revision": "dd761f6ab4a16bba7c068b8229d1f1b5",
+    "url": "/Fortune/static/media/mastercard1.dd761f6a.png"
+  }
+]);
